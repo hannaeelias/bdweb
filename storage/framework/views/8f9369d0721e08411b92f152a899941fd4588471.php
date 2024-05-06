ab7a7a4a6339beb7daae228655f5a7090c7866f6
hannae elias <hannae.elias@outlook.fr>
2024-05-06 14:45:36 +0200

    set up

---
+++ storage/framework/views/8f9369d0721e08411b92f152a899941fd4588471.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+  
+    </head>
+    <body>
+        <h1>login system</h1>
+        <li>Users can log in
+Visitors can create a new account
+Users may or may not be an administrator
+Only an administrator can promote another user to administrator status (or create a new user that is an admin)</li>
+        <h1>Profile page</h1>
+        <h1>Latest news</h1>
+        <h1>FAQ page</h1>
+        <h1>Contact page</h1>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\arikaro\resources\views/welcome.blade.php ENDPATH**/ ?>
